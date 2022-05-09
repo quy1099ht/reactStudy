24217bf525b2f5f37be0ecd2f00e0959cebe9f70
quy1099ht <quy.tran@bstarsolutions.com>
2022-05-09 14:45:22 +0700

    Finishing all basic function 9/5/2022!

--- src/Contents/Home.js
+++ src/Contents/Home.js
@@ -1,67 +1,110 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import "../CSS/ToDo.css"
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import "../CSS/ToDo.css";
+import axios from "axios";
 
-Home.propTypes = {
-
-};
+Home.propTypes = {};
 
 function Home(props) {
     const [arr, setArr] = useState([]);
     const [taskName, setTaskName] = useState("");
 
     useEffect(() => {
-        // .then((response) => {
-        //     console.log(response.data);
-        // })
-        getAPI()
-    }, [])
+        getAPI();
+    }, [arr]);
 
+    // useEffect(() => {
+    //     setArr(arr)
+
+    // }, [arr])
+    const cloneUnFinishedArr = (arr) => {
+        const array = []
+        arr.forEach(element => {
+            if (!element.isCompleted) {
+                array.push(element)
+            }
+        });
+        
+        console.log("🚀 ~ file: Home.js ~ line 22 ~ cloneUnFinishedArr ~ array", array)
+        return array;
+    }
+    const getLatestTask = (arr) => {
+        return arr[arr.length - 1];
+    }
     const changeTaskName = (e) => {
         setTaskName(e.target.value);
-    }
+    };
 
     const addTask = async (e) => {
-        e.preventDefault()
-        const res = await axios.post('http://localhost:8080/addTask', {
-            taskname: taskName
-        })
+        e.preventDefault();
+        const res = await axios.post("http://localhost:8080/addTask", {
+            taskname: taskName,
+        });
+        setTaskName("");
+        getAPI();
+    };
 
+    const completeTask = (e) => {
+        console.log(e.target.innerHTML);
+        const res = axios.post("http://localhost:8080/completeTask", {
+            taskname: e.target.innerHTML,
+        });
+        setArr(arr);
     }
-
 
     const getAPI = async () => {
-        const res = await axios.get('http://localhost:8080')
-        console.log("🚀 ~ file: Home.js ~ line 36 ~ getAPI ~ res", res.data)
-        setArr(res.data)
-        console.log(arr);
+        const res = await axios.get("http://localhost:8080");
+        var array = [];
+        setArr(res.data);
+    };
+    const removeTask = () => {
+        const task = getLatestTask(cloneUnFinishedArr(arr))
+
+        const body={
+            taskId : task._id
+        }
+        console.log("🚀 ~ file: Home.js ~ line 63 ~ removeTask ~ task._id", task._id)
+        axios.delete("http://localhost:8080/deleteTask",{ data: body});
         
-    }
+    };
 
     return (
         <div class="ToDoList">
             <h2> Simple Todo app </h2>
             <form onSubmit={addTask}>
-                <input type="text" onChange={changeTaskName} name="newtask" placeholder="add new task" />
+                <input
+                    value={taskName}
+                    type="text"
+                    onChange={changeTaskName}
+                    name="newtask"
+                    placeholder="add new task"
+                />
 
                 <button type="submit"> Add Task </button>
             </form>
             <h2> Added Task </h2>
             <div>
-
+                <p>Click to change unfinished task to finished status</p>
+                {arr.map((item, key) => {
+                    if (!item.isCompleted) {
+                        return <li onClick={completeTask}>{item.name}</li>;
+                    }
+                })}
             </div>
-            {[1,2,3,4].map((item, key) => {
-                return (
-                    <li key={key}>{item}</li>
-                )
-            })}
-
-
-            <button > Remove </button>
+            <br />
+            <button onClick={removeTask}>Remove Latest Task</button>
 
             <h2> Completed task </h2>
-            <li><input type="checkbox" checked />Done </li>
+            {arr.map((item) => {
+                if (item.isCompleted) {
+                    return (
+                        <li>
+                            <input disabled type="checkbox" checked />
+                            {item.name}{" "}
+                        </li>
+                    );
+                }
+            })}
         </div>
     );
 }
